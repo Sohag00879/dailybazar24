c13a84da37142fc73c03770f31ed06d752c1eeb5
sohag <shishirsohagislam@gmail.com>
2025-04-30 14:57:54 +0600

    finally done project

--- src/components/UI/ProductEditForm.tsx
+++ src/components/UI/ProductEditForm.tsx
@@ -48,8 +48,6 @@ const ProductEditForm = ({ productData, categoriesData }: { productData: IProduc
         }
     };
 
-
-
     return (
         <div className="container">
             <h1 className='title'>Edit Product</h1>
@@ -69,11 +67,11 @@ const ProductEditForm = ({ productData, categoriesData }: { productData: IProduc
                             <Input />
                         </Form.Item>
 
-                        <Form.Item name="sku" label="SKU">
+                        <Form.Item name="sku" label="SKU" rules={[{ required: true, message: 'SKU is required' }]}>
                             <Input />
                         </Form.Item>
 
-                        <Form.Item name="category" label="Category">
+                        <Form.Item name="category" label="Category" rules={[{ required: true, message: 'Category is required' }]}>
                             <Select>
                                 {categoriesData?.map(cat => (
                                     <Select.Option key={cat._id} value={cat.name}>
@@ -83,23 +81,23 @@ const ProductEditForm = ({ productData, categoriesData }: { productData: IProduc
                             </Select>
                         </Form.Item>
 
-                        <Form.Item name="price" label="Price">
+                        <Form.Item name="price" label="Price" rules={[{ required: true, message: 'Price is required' }]}>
                             <InputNumber style={{ width: '100%' }} />
                         </Form.Item>
 
-                        <Form.Item name="discountPercentage" label="Discount (%)">
+                        <Form.Item name="discountPercentage" label="Discount (%)" rules={[{ required: true, message: 'Discount is required' }]}>
                             <InputNumber style={{ width: '100%' }} />
                         </Form.Item>
 
-                        <Form.Item name="stock" label="Stock">
+                        <Form.Item name="stock" label="Stock" rules={[{ required: true, message: 'Stock is required' }]}>
                             <InputNumber style={{ width: '100%' }} />
                         </Form.Item>
 
-                        <Form.Item name="availabilityStatus" label="Availability Status">
+                        <Form.Item name="availabilityStatus" label="Availability Status" rules={[{ required: true, message: 'Availability is required' }]}>
                             <Input />
                         </Form.Item>
 
-                        <Form.Item name="minimumOrderQuantity" label="Min Order Quantity">
+                        <Form.Item name="minimumOrderQuantity" label="Min Order Quantity" rules={[{ required: true, message: 'Min Order is required' }]}>
                             <InputNumber style={{ width: '100%' }} />
                         </Form.Item>
                     </div>
@@ -109,48 +107,48 @@ const ProductEditForm = ({ productData, categoriesData }: { productData: IProduc
                                 <Button icon={<UploadOutlined />}>Select File</Button>
                             </Upload>
                         </Form.Item>
-                        <Form.Item name="tags" label="Tags (comma separated)">
+                        <Form.Item name="tags" label="Tags (comma separated)" rules={[{ required: true, message: 'Tags is required' }]}>
                             <Input />
                         </Form.Item>
-                        <Form.Item name="warrantyInformation" label="Warranty Info">
+                        <Form.Item name="warrantyInformation" label="Warranty Info" rules={[{ required: true, message: 'Waranty is required' }]}>
                             <Input />
                         </Form.Item>
 
                     </div>
 
-                    <Form.Item name="description" label="Description">
+                    <Form.Item name="description" label="Description" rules={[{ required: true, message: 'Description is required' }]}>
                         <TextArea rows={3} />
                     </Form.Item>
 
                     <div className="grid-3col">
-                        <Form.Item name={['meta', 'barcode']} label="Barcode">
+                        <Form.Item name={['meta', 'barcode']} label="Barcode" rules={[{ required: true, message: 'Barcode is required' }]}>
                             <Input />
                         </Form.Item>
-                        <Form.Item name="returnPolicy" label="Return Policy">
+                        <Form.Item name="returnPolicy" label="Return Policy" rules={[{ required: true, message: 'Return Policy is required' }]}>
                             <Input />
                         </Form.Item>
-                        <Form.Item name="shippingInformation" label="Shipping Info">
+                        <Form.Item name="shippingInformation" label="Shipping Info" rules={[{ required: true, message: 'Shipping Info is required' }]}>
                             <Input />
                         </Form.Item>
                     </div>
 
                     <div className="grid-3col">
-                        <Form.Item name={['dimensions', 'width']} label="Width">
+                        <Form.Item name={['dimensions', 'width']} label="Width" rules={[{ required: true, message: 'Width is required' }]}>
                             <InputNumber style={{ width: '100%' }} />
                         </Form.Item>
-                        <Form.Item name={['dimensions', 'height']} label="Height">
+                        <Form.Item name={['dimensions', 'height']} label="Height" rules={[{ required: true, message: 'Dimension  is required' }]}>
                             <InputNumber style={{ width: '100%' }} />
                         </Form.Item>
-                        <Form.Item name={['dimensions', 'depth']} label="Depth">
+                        <Form.Item name={['dimensions', 'depth']} label="Depth" rules={[{ required: true, message: 'Depth is required' }]}>
                             <InputNumber style={{ width: '100%' }} />
                         </Form.Item>
                     </div>
 
                     <div className='grid-3col'>
-                        <Form.Item name="thumbnail" label="Thumbnail URL">
+                        <Form.Item name="thumbnail" label="Thumbnail URL" rules={[{ required: true, message: 'Thumbnail is required' }]}>
                             <Input />
                         </Form.Item>
-                        <Form.Item name="weight" label="Weight">
+                        <Form.Item name="weight" label="Weight" rules={[{ required: true, message: 'Weight is required' }]}>
                             <Input />
                         </Form.Item>
                     </div>
